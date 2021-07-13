42e40a32fa0d5057f1f4f77b7773b8c8287bc1b0
laurariv01 <laura_riv01@yahoo.com>
2021-07-13 09:05:56 -0500

    This is my amazon replica

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //BEM Convention
+    <div className="app">
+
+
+    {/* Header */}
+
+      <Header/>
+
+
+
+    {/* Home */}
+    <h1>check it out! Laurazon has a logo and search bar! </h1>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCw3c-vsj01lAggIO9ISc0w1KIH4TDQwXw",
+    authDomain: "clone-58f8e.firebaseapp.com",
+    projectId: "clone-58f8e",
+    storageBucket: "clone-58f8e.appspot.com",
+    messagingSenderId: "35133817938",
+    appId: "1:35133817938:web:4022bac6733958db2bcb8b",
+    measurementId: "G-THVW7J8LVH"
+  };
